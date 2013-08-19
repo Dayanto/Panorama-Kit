d3534f9666ba2bc1ac7cdef7c38e378de71ded7f
Dayanto <3Dayanto@gmail.com>
2013-08-19 20:51:02 +0200

    With the new way of rendering, separate ticks aren't necessary anymore.

---
+++ src/panoramakit/core/OnFrameTickHandler.java
@@ -0,0 +1,34 @@
+package panoramakit.core;
+
+import java.util.EnumSet;
+import panoramakit.Dispatcher;
+import cpw.mods.fml.common.ITickHandler;
+import cpw.mods.fml.common.TickType;
+
+/**
+ * ClientTickHandler
+ * 
+ * @author dayanto
+ */
+public class OnFrameTickHandler implements ITickHandler {
+
+	@Override
+	public void tickStart(EnumSet<TickType> type, Object... tickData) {
+		Dispatcher.runTick();
+	}
+
+	@Override
+	public void tickEnd(EnumSet<TickType> type, Object... tickData) {
+	}
+
+	@Override
+	public EnumSet<TickType> ticks() {
+		return EnumSet.of(TickType.CLIENT);
+	}
+
+	@Override
+	public String getLabel() {
+		return "panoramakit_client_tick";
+	}
+
+}
