8b9203a7968317cdac94e13c5ac39715f9f57d29
Dayanto <3Dayanto@gmail.com>
2013-08-19 20:51:02 +0200

    Changed to a single tick system. (See previous commit)

--- src/panoramakit/core/ClientProxy.java
+++ src/panoramakit/core/ClientProxy.java
@@ -5,21 +5,14 @@ import cpw.mods.fml.relauncher.Side;
 
 /**
  * ClientProxy
- *  
+ * 
  * @author dayanto
- * @license GNU Lesser General Public License v3 (http://www.gnu.org/licenses/lgpl.html)
- *
  */
-public class ClientProxy extends CommonProxy
-{
+public class ClientProxy extends CommonProxy {
 	@Override
-	public void registerHandlers()
-	{
-		RenderTickHandler renderTick = new RenderTickHandler();
-		ClientTickHandler clientTick = new ClientTickHandler();
-		TickRegistry.registerTickHandler(renderTick, Side.CLIENT);
-		TickRegistry.registerTickHandler(clientTick, Side.CLIENT);
+	public void registerHandlers() {
+		OnFrameTickHandler onFrameTick = new OnFrameTickHandler();
+		TickRegistry.registerTickHandler(onFrameTick, Side.CLIENT);
 	}
-	
 
 }
--- src/panoramakit/core/CommonProxy.java
+++ src/panoramakit/core/CommonProxy.java
@@ -2,12 +2,10 @@ package panoramakit.core;
 
 /**
  * CommonProxy
- *  
+ * 
  * @author dayanto
- * @license GNU Lesser General Public License v3 (http://www.gnu.org/licenses/lgpl.html)
- *
  */
-public class CommonProxy
-{
-	public void registerHandlers() {}
+public class CommonProxy {
+	public void registerHandlers() {
+	}
 }
