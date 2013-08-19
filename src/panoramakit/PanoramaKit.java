a30a069b243741c8d8355321ca139c6a90567b73
Dayanto <3Dayanto@gmail.com>
2013-08-19 20:51:02 +0200

    Made some changes to set up the mod for 1.6.*.

--- src/panoramakit/PanoramaKit.java
+++ src/panoramakit/PanoramaKit.java
@@ -3,28 +3,24 @@ package panoramakit;
 import panoramakit.core.CommonProxy;
 import panoramakit.lib.Reference;
 import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
 
 /**
  * PanoramaKit
- *  
+ * 
  * @author dayanto
- * @license GNU Lesser General Public License v3 (http://www.gnu.org/licenses/lgpl.html)
- *
  */
 @Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
 @NetworkMod(clientSideRequired = true, serverSideRequired = false)
-public class PanoramaKit
-{	
+public class PanoramaKit {
 	@SidedProxy(clientSide = "panoramakit.core.ClientProxy", serverSide = "panoramakit.core.CommonProxy")
 	public static CommonProxy proxy;
-	
-	@Init
-	public void init(FMLInitializationEvent event)
-	{
+
+	@EventHandler
+	public void init(FMLInitializationEvent event) {
 		proxy.registerHandlers();
 	}
 }
--- src/panoramakitcore/setup/PKCPluginLoader.java
+++ src/panoramakitcore/setup/PKCPluginLoader.java
@@ -1,40 +1,41 @@
 package panoramakitcore.setup;
 
 import java.util.Map;
-
 import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
+import cpw.mods.fml.relauncher.IFMLLoadingPlugin.MCVersion;
 import cpw.mods.fml.relauncher.IFMLLoadingPlugin.TransformerExclusions;
 
 /**
  * PKCPluginLoader
- *  
+ * 
  * @author dayanto
- * @license GNU Lesser General Public License v3 (http://www.gnu.org/licenses/lgpl.html)
- *
  */
-@TransformerExclusions({"panoramakitcore", "panoramakitcore.asm", "panoramakitcore.setup"})
-public class PKCPluginLoader implements IFMLLoadingPlugin
-{
-	
+@TransformerExclusions({ "panoramakitcore" })
+@MCVersion("1.5.1")
+public class PKCPluginLoader implements IFMLLoadingPlugin {
+
 	@Override
-	public String[] getASMTransformerClass()
-	{
-		return new String[] {"panoramakitcore.asm.PKCTransformer"};
+	public String[] getASMTransformerClass() {
+		return new String[] { "panoramakitcore.asm.CodeTransformer" };
 	}
 
 	@Override
-	public String getModContainerClass()
-	{
+	public String getModContainerClass() {
 		return "panoramakitcore.setup.PKCModContainer";
 	}
-	
-	@Override
-	public String[] getLibraryRequestClass() { return null; }
 
 	@Override
-	public String getSetupClass() { return null; }
+	public String[] getLibraryRequestClass() {
+		return null;
+	}
 
 	@Override
-	public void injectData(Map<String, Object> data) {}
+	public String getSetupClass() {
+		return null;
+	}
+
+	@Override
+	public void injectData(Map<String, Object> data) {
+	}
 
 }
